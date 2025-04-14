655f02945fe60ed39d4a2aeb97f04993f6541eac
Gabilorium <chgabiv@gmail.com>
2025-04-14 23:58:40 +0000

    terminado

--- client/src/client.c
+++ client/src/client.c
@@ -37,6 +37,8 @@ int main(void)
 	// No olvides liberar el config antes de continuar
 	//config_destroy(config);
 
+
+
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
 	leer_consola(logger);
@@ -46,13 +48,14 @@ int main(void)
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// Creamos una conexión hacia el servidor
-	conexion = crear_conexion(ip, puerto);
+		conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-	enviar_mensaje(conexion, valor);
+		enviar_mensaje(valor, conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
+	
 
 	terminar_programa(conexion, logger, config);
 
@@ -75,11 +78,12 @@ t_log* iniciar_logger(void)
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config= config_create("cliente.config");
+
+	// Verificamos que el archivo de configuración tenga las claves necesarias
 	if (nuevo_config == NULL) {
 		perror("Error al intentar cargar el config");
 		exit(EXIT_FAILURE); // Finaliza el programa si no se pudo crear el config
 	}
-	// Verificamos que el archivo de configuración tenga las claves necesarias
 	/*if (config_has_property(nuevo_config, "IP") == false || config_has_property(nuevo_config, "PUERTO") == false || config_has_property(nuevo_config, "CLAVE") == false) {
 		printf("No se encontró la clave IP, PUERTO o CLAVE en el archivo de configuración\n");
 		exit(1);
@@ -91,8 +95,6 @@ t_config* iniciar_config(void)
 void leer_consola(t_log* logger)
 {
 	char* leido;
-	int result;
-
 	// La primera te la dejo de yapa
 	leido = readline("> ");
 	log_info(logger, ">> %s", leido);
@@ -110,23 +112,30 @@ void leer_consola(t_log* logger)
 void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
-	char* leido;
-	t_paquete* paquete;
+	char* leido = NULL;
+	t_paquete* paquete= crear_paquete();
 
-	// Leemos y esta vez agregamos las lineas al paquete
-	paquete = crear_paquete();
+	// Leemos y esta vez agregamos las lineas al paquete 
 	leido = readline("> ");
 
+	while(strcmp(leido, "") != 0){
+		agregar_a_paquete(paquete, leido, strlen(leido)+1);
+		free(leido);
+		leido = readline("> ");
+	}
+	enviar_paquete(paquete, conexion);
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	free(paquete);
-}
+	free(leido);
+	eliminar_paquete(paquete);
 
+}
+  
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	log_destroy(logger);
 	config_destroy(config);
-	close(conexion);
+	liberar_conexion(conexion);
 	exit(0);
 }
